da2978aaa741fa468d73ed4cd856e17c721f9007
Erik Truckner <eriktruckner@comcast.net>
2023-05-15 09:56:34 -0400

    completed cam anis

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import { Sphere } from '@react-three/drei'
+import { Sphere, Stars } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
+
 import CameraPositionLogger from './hooks/CameraPositionLogger'
 import Box from './Box'
 import Ball from './Ball'
@@ -20,10 +21,12 @@ export default function App({ event }) {
         far: 1000,
         position: [0, 0, 9],
       }}>
+      <color attach='background' args={['black']} />
       {/* <OrbitControls /> */}
       <CameraPositionLogger event='mousedown' />
-      <ambientLight />
-
+      <Stars />
+      <spotLight intensity={1} position={(0, 0, 9)} />
+      <ambientLight intensity={0.1} />
       <Box position={[9, 0, 0]} color='green' />
     </Canvas>
   )
--- src/Box.jsx
+++ src/Box.jsx
@@ -2,6 +2,7 @@ import { useRef, useState } from 'react'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import * as THREE from 'three'
+import * as TWEEN from '@tweenjs/tween.js'
 
 function Box({ position, color }) {
   // This reference will give us direct access to the mesh
@@ -30,14 +31,57 @@ function Box({ position, color }) {
   // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((state, delta) => {
     mesh.current.rotation.x += delta
+    //
+    TWEEN.update()
 
-    const originalCameraLookAt = new THREE.Vector3(0, 0, 0)
+    const cameraTargetPosition = mesh.current.position
+    const cameraTargetPositionOffset = new THREE.Vector3(
+      cameraTargetPosition.x + 10,
+      cameraTargetPosition.y + 2,
+      cameraTargetPosition.z + 5
+    )
 
     if (cameraMoved) {
-      camera.lookAt(mesh.current.position)
+      new TWEEN.Tween(cameraPosition)
+        .to(cameraTargetPositionOffset, 1000)
+        .easing(TWEEN.Easing.Linear.None)
+        .onUpdate(() => {
+          setCameraPosition(cameraPosition)
+        })
+        .start()
+      //
+      // Create a new tween for cameraTarget lookAt
+      new TWEEN.Tween(cameraTarget)
+        .to(cameraTargetPosition, 1000)
+        .easing(TWEEN.Easing.Linear.None)
+        .onUpdate(() => {
+          setCameraTarget(cameraTarget)
+        })
+        .start()
     } else {
-      camera.lookAt(originalCameraLookAt)
+      const originalCameraLookAt = new THREE.Vector3(0, 0, 0)
+      const originalCameraPosition = new THREE.Vector3(0, 0, 9)
+
+      new TWEEN.Tween(cameraPosition)
+        .to(originalCameraPosition, 1000)
+        .easing(TWEEN.Easing.Linear.None)
+        .onUpdate(() => {
+          setCameraPosition(cameraPosition)
+        })
+        .start()
+
+      // Create a new tween for cameraTarget
+      new TWEEN.Tween(cameraTarget)
+        .to(originalCameraLookAt, 1000)
+        .easing(TWEEN.Easing.Linear.None)
+        .onUpdate(() => {
+          setCameraTarget(cameraTarget)
+        })
+        .start()
     }
+    camera.position.copy(cameraPosition)
+    camera.lookAt(cameraTarget)
+    camera.updateProjectionMatrix()
   })
 
   // console.log(mesh)
@@ -54,7 +98,7 @@ function Box({ position, color }) {
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}>
       <boxGeometry args={[1, 1, 1]} />
-      <meshBasicMaterial color={color} />
+      <meshPhongMaterial color={color} />
     </mesh>
   )
 }
